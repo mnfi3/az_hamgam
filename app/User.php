dfc343a1e18dc0435ed69bbc897e2a220e256091
mohsen340 <mohsenfarjami340@gmail.com>
2019-08-26 13:17:38 +0430

    init project

---
+++ app/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name', 'mobile', 'student_number', 'national_code', 'field_id', 'role', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+}
